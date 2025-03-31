afe6f9f6c211e33c5252466b2aa44ae1eeb33828
YubeRodri <yrodriguez@uamv.edu.ni>
2025-03-31 11:00:01 -0600

    semana 3

---
+++ example1.py
@@ -0,0 +1,34 @@
+import statistics
+class Edad:
+    def _init_(self, edades):
+        self.edades = edades
+    
+    def calcular_media(self):
+        return sum(self.edades) / len(self.edades)
+    
+    def mostrar_media(self):
+        return f"La media de las edades es: {self.calcular_media():.2f}"
+    
+    def calc_media(self):
+        return statistics.mean(self.edades)
+
+def main():
+    edades = []
+    
+    while True:
+        try:
+            edad = int(input("Ingrese una edad (0 para terminar) => "))
+            if edad == 0:
+                break
+            edades.append(edad)
+        except ValueError:
+            print("Error: Debe ingresar un número válido")
+    if(not edades):
+        print("No se ingresaron edades")
+        return
+    edad_obj = Edad(edades)
+    print(edad_obj.mostrar_media())
+    print(f"Media calculada con statiscs: {edad_obj.calc_media():.2f}")
+    
+if __name__ == "_main_":
+    main()
